16c4dcd110e37d189e80b3472f8ec15fcdd408ce
wady11 <45234823+wady11@users.noreply.github.com>
2019-12-05 09:29:48 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,76 +1,68 @@
 'use strict';
 var arreglo = [];
 var game = {
-  user:'',
-  user2: '',
-  jugadoractual:'',
-  move:0
+    user: '',
+    user2: '',
+    jugadoractual: '',
+    move: 0
 };
 
+function setFig(id) {
 
-function setFig(id){
+    if (id === "x") {
 
-  if (id === "x") {
+        game.user = 'x';
+        $(`#${id}`).css('color', 'red');
+        $('#0').css('color', 'black');
 
-      game.user = 'x';
-      $(`#${id}`).css('color','red');
-      $('#0').css('color','black');
-   
-      $(`#x`).removeAttr('onClick');
-      $('#0').attr('onClick');
-         game.jugadoractual = id;
-      //console.log(game.user)
-      
-  } 
-   else if(id === "0"){
+        $(`#x`).removeAttr('onClick');
+        $('#0').attr('onClick');
+        game.jugadoractual = id;
+        //console.log(game.user)
 
-    game.user = '0';
-    $(`#${id}`).css('color','red');
-    $('#x').css('color','black');
-     $(`#0`).removeAttr('onClick');
-     $('#x').attr('onClick');
+    } else if (id === "0") {
 
-     game.jugadoractual = id;
-     
-  }
+        game.user = '0';
+        $(`#${id}`).css('color', 'red');
+        $('#x').css('color', 'black');
+        $(`#0`).removeAttr('onClick');
+        $('#x').attr('onClick');
+
+        game.jugadoractual = id;
+
+    }
 }
 
-
-
-function operacion(currentplay)
-  {
-      if(game.jugadoractual == ""){
+function operacion(currentplay) {
+    if (game.jugadoractual == "") {
         alert("Debe Elegir X u O")
-      }
-      else{
+    } else {
 
         if (game.jugadoractual === 'x') {
-            $(`#${currentplay}`).html(`<img src="/img/${game.jugadoractual}.png"/> `);//muestro la imagen
-            $(`#${currentplay}`).removeAttr('onClick');//remuevo el atributo 
-            $(`#${currentplay}`).attr("class",`${game.jugadoractual}`) 
-           
+            $(`#${currentplay}`).html(`<img src="/img/${game.jugadoractual}.png"/> `); //muestro la imagen
+            $(`#${currentplay}`).removeAttr('onClick'); //remuevo el atributo 
+            $(`#${currentplay}`).attr("class", `${game.jugadoractual}`)
+
             game.move++;
 
-
-            $(`#0`).css('color','red');
-            $('#x').css('color','black');
+            $(`#0`).css('color', 'red');
+            $('#x').css('color', 'black');
             game.user = 'x';
             game.jugadoractual = '0';
             gamestatus();
             draw(game.move);
             arreglo.push(currentplay);
 
-        }
-        else if(game.jugadoractual === '0'){
-          $(`#${currentplay}`).html(`<img src="/img/${game.jugadoractual}.png"/> `);
-          $(`#${currentplay}`).removeAttr('onClick');
-          $(`#${currentplay}`).attr("class",`${game.jugadoractual}`) ;
-            
-          game.move++;
+        } else if (game.jugadoractual === '0') {
+            $(`#${currentplay}`).html(`<img src="/img/${game.jugadoractual}.png"/> `);
+            $(`#${currentplay}`).removeAttr('onClick');
+            $(`#${currentplay}`).attr("class", `${game.jugadoractual}`);
 
-          $(`#0`).css('color','black');
-          $('#x').css('color','red');
-          game.user = '0';
+            game.move++;
+
+            $(`#0`).css('color', 'black');
+            $('#x').css('color', 'red');
+            game.user = '0';
             game.jugadoractual = 'x';
             gamestatus();
             draw(game.move);
@@ -78,121 +70,108 @@ function operacion(currentplay)
         }
     }
 
-  }
+}
 
+function gamestatus() {
+    var player = "";
 
-  function gamestatus(){
-    var player = "" ;
+    if (game.user == 'x') {
+        player = game.user;
+        console.log(player);
 
-    if(game.user == 'x')
-    { 
-      player = game.user;
-     console.log(player);
-
+    } else if (game.user == '0') {
+        player = game.user;
+        console.log(player);
     }
-    else if(game.user == '0'){
-      player = game.user;
-      console.log(player);
-    }
-    
 
-
-    switch(true){
+    switch (true) {
 
         //1
-        case $('#first').attr("Class") == player && $('#fifth').attr("Class") == player && $('#ninth').attr("Class") == player   :
+        case $('#first').attr("Class") == player && $('#fifth').attr("Class") == player && $('#ninth').attr("Class") == player:
 
-          Winner('#first','#fifth','#ninth',player);
-        break;
+            Winner('#first', '#fifth', '#ninth', player);
+            break;
 
-        //2
-        case $('#first').attr("Class") == player && $('#fouth').attr("Class") == player && $('#seventh').attr("Class") == player   :
+            //2
+        case $('#first').attr("Class") == player && $('#fouth').attr("Class") == player && $('#seventh').attr("Class") == player:
 
-          Winner('#first','#fouth','#seventh',player);
-          
-        break;
+            Winner('#first', '#fouth', '#seventh', player);
 
+            break;
 
-        //3
-        case $('#first').attr("Class") == player && $('#second').attr("Class") == player && $('#third').attr("Class") == player   :
+            //3
+        case $('#first').attr("Class") == player && $('#second').attr("Class") == player && $('#third').attr("Class") == player:
 
-          Winner('#first','#second','#third',player);
-          
-        break;
+            Winner('#first', '#second', '#third', player);
 
+            break;
 
-        //4
-        case $('#fouth').attr("Class") == player && $('#fifth').attr("Class") == player && $('#sixth').attr("Class") == player   :
+            //4
+        case $('#fouth').attr("Class") == player && $('#fifth').attr("Class") == player && $('#sixth').attr("Class") == player:
 
-          Winner('#fouth','#fifth','#sixth',player);
-        break;
+            Winner('#fouth', '#fifth', '#sixth', player);
+            break;
 
+            //5
+        case $('#seventh').attr("Class") == player && $('#eighth').attr("Class") == player && $('#ninth').attr("Class") == player:
 
-        //5
-        case $('#seventh').attr("Class") == player && $('#eighth').attr("Class") == player && $('#ninth').attr("Class") == player   :
+            Winner('#seventh', '#eighth', '#ninth', player);
 
-          Winner('#seventh','#eighth','#ninth',player);
-          
-        break;
+            break;
 
-        //6
-        case $('#third').attr("Class") == player && $('#sixth').attr("Class") == player && $('#ninth').attr("Class") == player   :
+            //6
+        case $('#third').attr("Class") == player && $('#sixth').attr("Class") == player && $('#ninth').attr("Class") == player:
 
-          Winner('#third','#sixth','#ninth',player);
-          
-        break;
+            Winner('#third', '#sixth', '#ninth', player);
 
-        //7
-        case $('#third').attr("Class") == player && $('#fifth').attr("Class") == player && $('#seventh').attr("Class") == player   :
+            break;
 
-          Winner('#third','#fifth','#seventh',player);
-          
-        break;
+            //7
+        case $('#third').attr("Class") == player && $('#fifth').attr("Class") == player && $('#seventh').attr("Class") == player:
 
-        //8
-        case $('#second').attr("Class") == player && $('#fifth').attr("Class") == player && $('#eighth').attr("Class") == player   :
+            Winner('#third', '#fifth', '#seventh', player);
 
-          Winner('#second','#fifth','#eighth',player);
-          
-        break;
+            break;
+
+            //8
+        case $('#second').attr("Class") == player && $('#fifth').attr("Class") == player && $('#eighth').attr("Class") == player:
+
+            Winner('#second', '#fifth', '#eighth', player);
+
+            break;
 
     }
-      
 
-  }
+}
 
+//funcion de los ganadores
+function Winner(valor1, valor2, valor3, player) {
 
-  //funcion de los ganadores
-  function Winner(valor1,valor2,valor3,player){
-
-    $(`${valor1}`).css('background-color', 'red');  
-    $(`${valor2}`).css('background-color', 'red'); 
-    $(`${valor3}`).css('background-color', 'red'); 
+    $(`${valor1}`).css('background-color', 'red');
+    $(`${valor2}`).css('background-color', 'red');
+    $(`${valor3}`).css('background-color', 'red');
 
     alert(`El jugador ${player} a ganado la partida`)
 
-  }
+}
 
-  //cuando termina en empate
-  function draw(number) {
-    if(number == 9){
-      $('.message').html("It's a Draw.");
+//cuando termina en empate
+function draw(number) {
+    if (number == 9) {
+        $('.message').html("It's a Draw.");
     }
-  }
+}
 
-  
-  function resetear(){
+function resetear() {
 
-      for(let increa = 0; increa < arreglo.length;increa++){
+    for (let increa = 0; increa < arreglo.length; increa++) {
 
-        $(`#${arreglo[increa]}`).attr('onClick','operacion(this.id)');
+        $(`#${arreglo[increa]}`).attr('onClick', 'operacion(this.id)');
         $(`#${arreglo[increa]}`).removeAttr('Class');
         $(`#${arreglo[increa]}`).css('background', 'white');
         $(`#${arreglo[increa]}`).html('');
         $(`.message`).html('');
         game.move = 0;
-      }
+    }
 
-    
-    
-  }
+}
